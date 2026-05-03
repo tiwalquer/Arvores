4a52f45cd7c6dc1b980366c7fab6a4721fcf1703
tiwalquer <tiagopas@edu.unifil.br>
2026-05-03 19:30:02 -0300

    Add files via upload

--- Arvore/ArvoreBinaria.java
+++ Arvore/ArvoreBinaria.java
@@ -15,14 +15,76 @@ public class ArvoreBinaria {
     public void inserir(Integer conteudo){
 
         No novoNo = new No(conteudo);
+        No aux = this.raiz;
 
         if(existeInicio()){
             this.raiz = novoNo;
 
-        }else if(this.raiz.getConteudo() > conteudo){ //esquerda
-
         }
+
+        if(novoNo.getConteudo() < aux.getConteudo()) {
+//            if (aux == null) {
+//
+//            }
+
+            cuEsquerdo(aux.getEsquerda());
+            aux = novoNo;
+        }
+
+        if(novoNo.getConteudo() > aux.getConteudo()) {
+//            if (aux == null) {
+//
+//            }
+
+            cuDireito(aux.getDireita());
+            aux = novoNo;
+        }
+
+
+
+
     }
+
+    public void exibir(){
+        No aux = this.raiz;
+
+        if(!existeInicio()){
+            return;
+        }
+
+         do{
+            cuEsquerdo(aux);
+            System.out.println(aux.getConteudo());
+        }while(aux.getEsquerda() != null);
+
+
+        do {
+            cuDireito(aux);
+            System.out.println(aux.getConteudo());
+        }while(aux.getDireita() != null);
+    }
+
+    private void cuEsquerdo(No aux){
+
+        if(aux == null){
+            return ;
+        }
+
+        cuEsquerdo( aux.getEsquerda() );
+        //return aux;
+    }
+
+    private No cuDireito(No aux){
+
+        if(aux == null){
+            return null;
+        }
+
+        cuDireito(  aux.getDireita() );
+        return aux;
+    }
+
+
 
     public boolean existeInicio(){
         if(this.raiz != null){
---
+++ Arvore/Main.java
@@ -0,0 +1,17 @@
+package Arvore;
+
+public class Main {
+
+    static void main(String[] args) {
+
+        ArvoreBinaria executarArvore = new ArvoreBinaria();
+
+        executarArvore.exibir();
+
+        executarArvore.inserir(17);
+
+        executarArvore.exibir();
+
+    }
+
+}
