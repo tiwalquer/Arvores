78ad4653bf0628a9ae4e4977c0331cd9e8ff7b2b
tiwalquer <tiagopas@edu.unifil.br>
2026-05-04 11:12:43 -0300

    Add files via upload

---
+++ Arvores/Main.java
@@ -0,0 +1,19 @@
+package Arvores;
+
+public class Main {
+
+
+
+   public static void main(String[] args) {
+
+        ArvoreBinaria executarArvore = new ArvoreBinaria();
+
+        executarArvore.exibir();
+
+//        executarArvore.inserir(17);
+//
+//        executarArvore.exibir();
+
+    }
+
+}
