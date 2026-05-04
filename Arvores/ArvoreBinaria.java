577e8bac7b1203e1ca9eb4cbe7598cedd9944a5a
tiwalquer <tiagopas@edu.unifil.br>
2026-05-04 16:16:44 -0300

    Add files via upload

--- Arvores/ArvoreBinaria.java
+++ Arvores/ArvoreBinaria.java
@@ -7,39 +7,48 @@ public class ArvoreBinaria {
 
     public ArvoreBinaria(){
 
-        this.raiz = new No(null);
-
+        this.raiz = null; //fazendo isso, nao cria um 'Nó', nao existe nada ainda, ela so sera criada no 'inserir' tornando comparacoes possiveis
+        //this.raiz = new No(null); // dessa forma nos criamos o 'Nó' porem o seu conteudo é vazio, inproprio para realizar comparacoes
     }
 
 
     //sao necessarios 2 metodos para usar a chamada recursiva, pois o " nó 'aux' " nao pode ser criado no metodo recursivo !!!
+    /**
+     *
+     * @param noAtual serve para nao nos perdermos dentro da arvore
+     * @param conteudo o conteudo do novo 'no' desejado
+     * @return o 1 retorno criara o novo 'Nó'  ...  o 2 retorno cuida da volta/ nao deixa a gente perde os valores, quando as chamadas recursivas vao voltando elas vao se deparando com o
+     * 'return noAtual' que vai 'setting the value' para o do 'Nó' que ele era por causo do 'noAtual.set ...algo '
+     */
     public No inserirRecursivamente(No noAtual, Integer conteudo){ // 'noAtual': faz o papel do 'auxiliar'
         if(noAtual == null){
 
-            return new No(conteudo);
+            return new No(conteudo); //cria o novo 'Nó'
         }
 
         if(noAtual.getConteudo() > conteudo){
-            return inserirRecursivamente( noAtual.getEsquerda(), conteudo);
+
+            noAtual.setEsquerda(inserirRecursivamente( noAtual.getEsquerda(), conteudo)); //sempre lembrar de atualizar os 'apontamentos' dos nós, se nao o valor é perdido
 
         }else if (noAtual.getConteudo() < conteudo){
-            return inserirRecursivamente( noAtual.getDireita(), conteudo);
+            noAtual.setDireita(inserirRecursivamente( noAtual.getDireita(), conteudo)); // seta o resultado disso = 'inserirRecursivamente( noAtual.getDireita(), conteudo)'
+                // o qual tem retorno = a isso 'noAtual'
+        }else{
+            System.out.println("Nós com conteudos(" + noAtual.getConteudo() + ") iguais nao sao permitidos!");
         }
-        System.out.println("Nós com conteudos iguais nao sao permitidos!");
-        return null;
+        return noAtual;
     }
 
     public void inserir(Integer conteudo){
 
-        inserirRecursivamente( this.raiz, conteudo);
+        raiz = inserirRecursivamente( this.raiz, conteudo); //o valor da raiz é trocado, porem no fim ele acaba o mesmo por causado 'return noAtual'
     }
 
 
     private void exibirRecursivamente(No noAtual){
 
-        if(!existeInicio()){
+        if(noAtual == null){ //por que aqui deve ter so o produto
             return;
-            // return null;
         }
 
         exibirRecursivamente(noAtual.getEsquerda());
@@ -52,29 +61,12 @@ public class ArvoreBinaria {
 
         No aux = this.raiz;
 
+        if(!existeInicio()){
+            return;
+        }
+
         exibirRecursivamente(aux);
     }
-
-    private void cuEsquerdo(No aux){
-
-        if(aux == null){
-            return ;
-        }
-
-        cuEsquerdo( aux.getEsquerda() );
-        //return aux;
-    }
-
-    private No cuDireito(No aux){
-
-        if(aux == null){
-            return null;
-        }
-
-        cuDireito(  aux.getDireita() );
-        return aux;
-    }
-
 
 
     public boolean existeInicio(){
--- Arvores/Main.java
+++ Arvores/Main.java
@@ -2,17 +2,19 @@ package Arvores;
 
 public class Main {
 
-
-
-   public static void main(String[] args) {
+    public static void main(String[] args) {
 
         ArvoreBinaria executarArvore = new ArvoreBinaria();
 
         executarArvore.exibir();
 
-//        executarArvore.inserir(17);
-//
-//        executarArvore.exibir();
+        executarArvore.inserir(17);
+        executarArvore.inserir(30);
+        executarArvore.inserir(30);
+        executarArvore.inserir(4);
+        executarArvore.inserir(-2);
+        executarArvore.inserir(678);
+        executarArvore.exibir();
 
     }
 
